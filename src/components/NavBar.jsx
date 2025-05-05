998e4ffe3a79b7a0c558d607415645cefb98629d
anik-das <anikdas1085@gmail.com>
2025-05-05 22:18:22 +0600

    Nav bar complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from "react";
+import React, {  useState } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/greencart_assets/assets";
 import { useAppContext } from "../context/AppContect";
@@ -16,7 +16,7 @@ const NavBar = () => {
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-      <NavLink to="/">
+      <NavLink to="/" onClick={()=>setOpen(false)}>
         <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
       </NavLink>
 
@@ -35,21 +35,32 @@ const NavBar = () => {
           <img src={assets.search_icon} className="w-4 h-4" alt="" />
         </div>
 
-        <div className="relative cursor-pointer">
-          <img src={assets.cart_icon} className="w-4 h-4" alt="" />
+        <div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
+          <img src={assets.cart_icon} className="w-5 h-4" alt="" />
           <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">
             0
           </button>
         </div>
 
         {!user ? (
-          <button className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm">
+          <button
+            onClick={() => setShowUserLogin(true)}
+            className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm"
+          >
             Login
           </button>
         ) : (
-          <button className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm">
-            Logout
-          </button>
+          <div className="relative group">
+            <img src={assets.profile_icon} className="w-10" alt="" />
+            <ul className="hidden group-hover:block  absolute top-10 right-0 bg-white shadow border-gray-200 py-2.5 w-28 rounded-md text-sm z-40">
+              <li onClick={()=>navigate("/myOrders")} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">
+                My Orders
+              </li>
+              <li onClick={logout} className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer">
+                Logout
+              </li>
+            </ul>
+          </div>
         )}
       </div>
 
@@ -63,40 +74,52 @@ const NavBar = () => {
       </button>
 
       {/* Mobile Menu */}
-      {open && ( <div
-        className={`${
-          open ? "flex" : "hidden"
-        } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
-      >
-        <NavLink to="/" onClick={() => setOpen(false)} className="block">
-          Home
-        </NavLink>
-        <NavLink to="products" onClick={() => setOpen(false)} className="block">
-          All Products
-        </NavLink>
-
-        {user && (
-          <NavLink to="/contact" onClick={() => setOpen(false)}>
-            My Orders
+      {open && (
+        <div
+          className={`${
+            open ? "flex" : "hidden"
+          } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+        >
+          <NavLink to="/" onClick={() => setOpen(false)} className="block">
+            Home
           </NavLink>
-        )}
-        <NavLink to="/contact" onClick={() => setOpen(false)}>
-          Contact
-        </NavLink>
+          <NavLink
+            to="products"
+            onClick={() => setOpen(false)}
+            className="block"
+          >
+            All Products
+          </NavLink>
 
-        {!user ? (
-          <button onClick={()=>{setOpen(false);
-            setShowUserLogin(true)
-          }} className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm">
-            Login
-          </button>
-        ) : (
-          <button onClick={logout()
-          } className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm">
-            Logout
-          </button>
-        )}
-      </div>)}
+          {user && (
+            <NavLink to="/contact" onClick={() => setOpen(false)}>
+              My Orders
+            </NavLink>
+          )}
+          <NavLink to="/contact" onClick={() => setOpen(false)}>
+            Contact
+          </NavLink>
+
+          {!user ? (
+            <button
+              onClick={() => {
+                setOpen(false);
+                setShowUserLogin(true);
+              }}
+              className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm"
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              onClick={logout}
+              className="cursor-pointer px-6 py-2 mt-2 bg-primary-dull hover:bg-primary transition text-white rounded-full text-sm"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
